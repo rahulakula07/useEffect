db0dcae6ed44ac372ef37ec778704c050d282487
rahul akula <rahulakula4265@gmail.com>
2025-02-21 03:18:48 +0530

    completted

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import "./App.css"
 
 function BasicExample() {
   return (
-    <Navbar expand="md" className="bg-body-tertiary" >
+    <Navbar  expand="md" className="maincontainer"  >
       <Container >
-        <Navbar.Brand href="#home">Rahu</Navbar.Brand>
+        <Navbar.Brand href="#home"><h2>FoodApp</h2></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" style={{justifyContent:'flex-end',gap:"20px"}}>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-          <Button >Login</Button>  
-          <Button>signup</Button>
+          <Button className='colo'>Login</Button>  
+          <Button className='colo'>signup</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/cards/cards.js
+++ src/cards/cards.js
@@ -3,26 +3,43 @@ import React, { useEffect,useState } from 'react'
 
 const Cards = () => {
     const[data,setdata]=useState([])
-// console.log(data);
+    const [filteredData, setFilteredData] = useState([])
+console.log(data);
     useEffect(()=>{
-        fetch("https://dummyjson.com/recipes").then(res=>res.json()).then(res=>setdata(res.recipes)).catch(err=>console.log(err))    
-    })
+        fetch("https://dummyjson.com/recipes").then(res=>res.json()).then(res=>{setdata(res.recipes)
+          setFilteredData(res.recipes)
+        }).catch(err=>console.log(err))    
+    },[])
 
-    const[filterdata,setfilterdata]=useState([])
 
-    // useEffect((res)=>{
-    //     data.filter()
-    // },[])
+    const handleFilter = (difficulty) => {
+      if (difficulty === "all") {
+        setFilteredData(data);
+      } else {
+        const filtered = data.filter((item) => item.difficulty.toLowerCase() === difficulty);
+        setFilteredData(filtered);
+      }
+    };
+   
 
 
     return (
-        <div>
-        <h1>Products</h1>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          {data.map((e) => {
+     
+        <div style={{width:"100%",display:'flex'}}>
+            <div id="left" style={{ width: "10%", display: "flex", flexDirection: "column", gap: "10px",marginTop:"100px",margin:"100px 70px 0 70px"}}>
+            <button onClick={() => handleFilter("all")} style={{backgroundColor:"pink",border:"none",borderRadius:"7px",padding:"7px 0px"}}>All</button>
+        <button onClick={() => handleFilter("easy")} style={{backgroundColor:"pink",border:"none",borderRadius:"7px",padding:"7px 0px"}}>Easy</button>
+        <button onClick={() => handleFilter("medium")} style={{backgroundColor:"pink",border:"none",borderRadius:"7px",padding:"7px 0px"}}>Medium</button>
+        
+        
+      </div>
+           <div style={{width:"88%"}}>
+           <h1 style={{paddingLeft:"500px"}}>Products</h1>  
+        <div style={{ display: 'flex', flexWrap: 'wrap' }} id='right'>
+          {filteredData.map((e) => {
             return (
-              <div key={e.id} style={{ width: "350px", height: "3auto", margin: "10px", border: "2px solid black",  textAlign: "center",borderRadius:"" }} >
-                <img src={e.image} alt={e.title} style={{ width: "100%", height: "60%" }} />
+              <div key={e.id} style={{ width: "350px", height: "3auto", margin: "10px", border: "2px solid black", padding:"10px", textAlign: "center",borderRadius:"10px" }} >
+                <img src={e.image} alt={e.title} style={{ width: "100%", height: "60%", borderRadius:"10px" }} />
                 <h3>name: ${e.name}</h3>
                 <p>Cuisine:{e.cuisine}</p>
                  <p>mealType:{e.mealType}</p>
@@ -31,6 +48,8 @@ const Cards = () => {
             );
           })}
         </div>
+           </div>
+       
       </div>
     );
   };            
